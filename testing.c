84b5133df48e28794bdf03a03fb3ea3e092856a8
josiah <josiahd.kitchin@gmail.com>
2025-04-16 19:21:37 -0700

    more lines

--- testing.c
+++ testing.c
@@ -1,3 +1,16 @@
 
+#include <stdio.h> 
 
 int main() {
+  for (int i = 0; i < 5; i++) { 
+    printf("Hello world\n");
+  } 
+
+  for (int i = 0; i < 5; i++) { 
+    printf("Hello world\n");
+  } 
+
+  for (int i = 0; i < 5; i++) { 
+    printf("Hello world\n");
+  } 
+}
